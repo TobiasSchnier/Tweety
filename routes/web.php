598376328535ee0c8b1c41459b604878c4a56343
Tobias Schnier <tobias@frielingsdorf-datenservice.de>
2020-08-25 20:05:38 +0200

    commit

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,6 @@ Route::middleware('auth')->group(function(){
 
   Route::post('/tweets/{tweet}/like', 'TweetLikesController@store');
   Route::delete('/tweets/{tweet}/like', 'TweetLikesController@destroy');
-  Route::test('/tweets/{tweet}/like', 'TweetLikesController@destroy');
 
 
   Route::post('/profiles/{user:username}/follow', 'FollowsController@store')->name('follow');
